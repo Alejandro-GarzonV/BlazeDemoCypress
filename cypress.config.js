3e49d4ab9c0b5c5fed92c12d511d7d71d8811e2f
Alejandro Garzón <117865764+Alejandro-GarzonV@users.noreply.github.com>
2024-06-26 13:13:27 -0500

    implementacion de allure report
    se implemento allure  report   command line y  rifraf   sin que falle el proyecto pero  la linea del fichero  e2e en el directorio support   no admite  la  improtacion de  report  adpter de mistyc  queda en revision  y comentariada

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,26 @@ const addCucumberPreprocessorPlugin =
   require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin =
   require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
 
 module.exports = defineConfig({
 
   viewportWidth: 1280,
   viewportHeight: 720,
   defaultCommandTimeout:60000,
+
+  env: {
+    allure: true,
+    allureResults: 'allure-results',
+    allureResultsWatchPath: 'allure-results',
+    allureLogCyCommands: true,
+    allureSkipCommands: 'screenshot,wait',
+    allureWrapCustomCommands: true,
+    allureCleanResults: false,
+    allureAttachRequests: false,
+    allureCompactAttachments: true,
+    allureAddVideoOnPass: false,
+  },
 
   e2e: {
     async setupNodeEvents(on, config) {
@@ -19,9 +33,8 @@ module.exports = defineConfig({
       });
       on("file:preprocessor", bundler);
       await addCucumberPreprocessorPlugin(on, config);
-      
+      configureAllureAdapterPlugins(on, config); // Integración de Allure Adapter
       return config;
-      
     },
     specPattern: "cypress/e2e/features/*.feature",
     baseUrl:"https://www.demoblaze.com/",
